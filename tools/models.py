ad2ef3d1e4703e0efcba9807eb335c86948ffae6
Morgan Humes <mhumes@cspire.com>
2016-04-21 12:41:01 -0500

    Role Based Access Controls

--- tools/models.py
+++ tools/models.py
@@ -25,6 +25,27 @@ class Process(models.Model):
     content = HStoreField(default={})
     status = models.PositiveSmallIntegerField(null=False, default=STATUS_SCHEDULED, choices=CHOICES_STATUS)
     exception = models.TextField()
+    view_permission = models.CharField(max_length=64, db_index=True)
 
     class Meta:
         ordering = ['-start_timestamp', '-end_timestamp']
+        permissions = (
+            ("process_call_park_pickup_configurator_exec", "Call Park/Pickup Configurator Execute"),
+            ("process_call_park_pickup_configurator_view", "Call Park/Pickup Configurator View Results"),
+            ("process_device_specific_migration_exec", "Device Specific Migration Execute"),
+            ("process_device_specific_migration_view", "Device Specific Migration View Results"),
+            ("process_firmware_report_exec", "Firmware Report Execute"),
+            ("process_firmware_report_view", "Firmware Report View Results"),
+            ("process_fraud_compliance_reset_exec", "Fraud Compliance Reset Tool Execute"),
+            ("process_fraud_compliance_reset_view", "Fraud Compliance Reset Tool View Results"),
+            ("process_lab_rebuild_exec", "Lab Rebuild Execute"),
+            ("process_lab_rebuild_view", "Lab Rebuild View Results"),
+            ("process_ptt_configurator_exec", "Push To Talk Configurator Execute"),
+            ("process_ptt_configurator_view", "Push To Talk Configurator View Results"),
+            ("process_registration_by_type_exec", "Registration by Type Report Execute"),
+            ("process_registration_by_type_view", "Registration by Type Report View Results"),
+            ("process_registration_report_exec", "Registration Report Execute"),
+            ("process_registration_report_view", "Registration Report View Results"),
+            ("process_tag_report_exec", "Tag Report Execute"),
+            ("process_tag_report_view", "Tag Report View Results"),
+        )
--- tools/views.py
+++ tools/views.py
@@ -5,12 +5,13 @@ import importlib
 # Django
 from django.utils import timezone
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
 from django.views.generic import TemplateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.http import HttpResponseRedirect, HttpResponse
-from django.core.urlresolvers import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 # Application
 import tools.forms
@@ -30,7 +31,15 @@ class ProcessListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         queryset = super(ProcessListView, self).get_queryset()
-        return queryset.defer("content")
+        # filter based upon permissions
+        permissions = []
+        for permission in ['tools.{}'.format(x) for x,y in Process._meta.permissions]:
+            if self.request.user.has_perm(permission):
+                permissions.append(permission)
+        queryset = queryset.filter(view_permission__in=permissions)
+        # defer content
+        queryset = queryset.defer("content")
+        return queryset
 
 
 class ProcessDetailView(LoginRequiredMixin, DetailView):
@@ -38,6 +47,8 @@ class ProcessDetailView(LoginRequiredMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(ProcessDetailView, self).get_context_data(**kwargs)
+        if not self.request.user.has_perm(context['object'].view_permission):
+            raise PermissionDenied
         context['files'] = dict()
         for key,value in context['object'].content.items():
             extension = key.split('.')[-1]
@@ -126,12 +137,12 @@ class ToolView(TemplateView):
                                              method=self.process_name,
                                              parameters=form.cleaned_data,
                                              start_timestamp=timezone.now(),
-                                             end_timestamp=None)
+                                             end_timestamp=None,
+                                             view_permission=self.permission_view)
         module = '.'.join(self.process_function.split('.')[:-1])
         method = self.process_function.split('.')[-1]
         importlib.import_module(module)
         process_function = eval(self.process_function)
-        # django_rq.enqueue(process_function, process_id=self.object.pk, timeout=0)
         q = rq.Queue('default', connection=Redis(), default_timeout=10800)
         q.enqueue_call(process_function, args=(self.object.pk,))
         return HttpResponseRedirect(self.get_success_url())
@@ -155,59 +166,82 @@ class ToolView(TemplateView):
         return url
 
 
-class DeviceSpecificMigrationToolView(LoginRequiredMixin, ToolView):
+class CallParkPickupConfiguratorToolView(PermissionRequiredMixin, LoginRequiredMixin, ToolView):
+    permission_required = 'tools.process_call_park_pickup_configurator_exec'
+    permission_view = 'tools.process_call_park_pickup_configurator_view'
+    process_name = 'Call Park/Pickup Configurator'
+    process_function = 'tools.jobs.call_park_pickup_configurator.call_park_pickup_configurator'
+    template_name = 'tools/call_park_pickup_configurator.html'
+    form_class = tools.forms.TypedProviderGroupForm
+
+
+class DeviceSpecificMigrationToolView(PermissionRequiredMixin, LoginRequiredMixin, ToolView):
+    permission_required = 'tools.process_device_specific_migration_exec'
+    permission_view = 'tools.process_device_specific_migration_view'
     process_name = 'Device Specific Migration'
     process_function = 'tools.jobs.device_specific_migration.device_specific_migration'
     template_name = 'tools/device_specific_migration_tool.html'
     form_class = tools.forms.TypedProviderGroupForm
 
 
-class FraudComplianceResetToolView(LoginRequiredMixin, ToolView):
+class FirmwareReportView(PermissionRequiredMixin, LoginRequiredMixin, ToolView):
+    permission_required = 'tools.process_firmware_report_exec'
+    permission_view = 'tools.process_firmware_report_view'
+    process_name = 'Firmware Report'
+    process_function = 'tools.jobs.firmware_report.firmware_report'
+    template_name = 'tools/firmware_report.html'
+    form_class = tools.forms.ProviderGroupForm
+
+
+class FraudComplianceResetToolView(PermissionRequiredMixin, LoginRequiredMixin, ToolView):
+    permission_required = 'tools.process_fraud_compliance_reset_exec'
+    permission_view = 'tools.process_fraud_compliance_reset_view'
     process_name = 'Fraud Compliance Reset'
     process_function = 'tools.jobs.fraud_compliance_reset.fraud_compliance_reset'
     template_name = 'tools/fraud_compliance_reset_tool.html'
     form_class = tools.forms.TypedProviderGroupForm
 
 
-class LabResetToolView(LoginRequiredMixin, ToolView):
+class LabResetToolView(PermissionRequiredMixin, LoginRequiredMixin, ToolView):
+    permission_required = 'tools.process_lab_rebuild_exec'
+    permission_view = 'tools.process_lab_rebuild_view'
     process_name = 'Lab Rebuild'
     process_function = 'tools.jobs.lab_rebuild.lab_rebuild'
     template_name = 'tools/lab_rebuild.html'
     form_class = tools.forms.LabRebuildForm
 
 
-class FirmwareReportView(LoginRequiredMixin, ToolView):
-    process_name = 'Firmware Report'
-    process_function = 'tools.jobs.firmware_report.firmware_report'
-    template_name = 'tools/firmware_report.html'
-    form_class = tools.forms.ProviderGroupForm
-
-class RegistrationReportView(LoginRequiredMixin, ToolView):
-    process_name = 'Registration Report'
-    process_function = 'tools.jobs.registration_report.registration_report'
-    template_name = 'tools/registration_report.html'
-    form_class = tools.forms.ProviderGroupForm
-
-class RegistrationByTypeReportView(LoginRequiredMixin, ToolView):
-    process_name = 'Registration By Type Report'
-    process_function = 'tools.jobs.registration_by_type.registration_by_type'
-    template_name = 'tools/registration_by_type.html'
-    form_class = tools.forms.EmptyForm
-
-class TagReportView(LoginRequiredMixin, ToolView):
-    process_name = 'Tag Report'
-    process_function = 'tools.jobs.tag_report.tag_report'
-    template_name = 'tools/tag_report.html'
-    form_class = tools.forms.TagReportForm
-
-class PushToTalkConfiguratorToolView(LoginRequiredMixin, ToolView):
+class PushToTalkConfiguratorToolView(PermissionRequiredMixin, LoginRequiredMixin, ToolView):
+    permission_required = 'tools.process_ptt_configurator_exec'
+    permission_view = 'tools.process_ptt_configurator_view'
     process_name = 'Push To Talk Configurator'
     process_function = 'tools.jobs.ptt_configurator.ptt_configurator'
     template_name = 'tools/ptt_configurator.html'
     form_class = tools.forms.TypedProviderGroupForm
 
-class CallParkPickupConfiguratorToolView(LoginRequiredMixin, ToolView):
-    process_name = 'Call Park/Pickup Configurator'
-    process_function = 'tools.jobs.call_park_pickup_configurator.call_park_pickup_configurator'
-    template_name = 'tools/call_park_pickup_configurator.html'
-    form_class = tools.forms.TypedProviderGroupForm
+
+class RegistrationByTypeReportView(PermissionRequiredMixin, LoginRequiredMixin, ToolView):
+    permission_required = 'tools.process_registration_by_type_exec'
+    permission_view = 'tools.process_registration_by_type_view'
+    process_name = 'Registration By Type Report'
+    process_function = 'tools.jobs.registration_by_type.registration_by_type'
+    template_name = 'tools/registration_by_type.html'
+    form_class = tools.forms.EmptyForm
+
+
+class RegistrationReportView(PermissionRequiredMixin, LoginRequiredMixin, ToolView):
+    permission_required = 'tools.process_registration_report_exec'
+    permission_view = 'tools.process_registration_report_view'
+    process_name = 'Registration Report'
+    process_function = 'tools.jobs.registration_report.registration_report'
+    template_name = 'tools/registration_report.html'
+    form_class = tools.forms.ProviderGroupForm
+
+
+class TagReportView(PermissionRequiredMixin, LoginRequiredMixin, ToolView):
+    permission_required = 'tools.process_tag_report_exec'
+    permission_view = 'tools.process_tag_report_view'
+    process_name = 'Tag Report'
+    process_function = 'tools.jobs.tag_report.tag_report'
+    template_name = 'tools/tag_report.html'
+    form_class = tools.forms.TagReportForm
