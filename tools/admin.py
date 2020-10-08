3ce173b12f28dde7a749455be0faa7ab551d56a8
James Bane <jamesbane0421@gmail.com>
2020-10-08 01:07:17 +0000

    displaying the Broadworks IDPT in the admin page

--- tools/admin.py
+++ tools/admin.py
@@ -4,5 +4,5 @@ from tools.models import DeviceType
 
 # Register your models here.
 class DeviceTypeViewAdmin(admin.ModelAdmin):
-    list_display = ('manufacturer', 'model')
+    list_display = ('manufacturer', 'model', 'identity_device_profile_type')
 admin.site.register(DeviceType, DeviceTypeViewAdmin)
