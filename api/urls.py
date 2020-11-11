4540b08fe53e23f01dcd6d7ff1534f0454e246d4
James Bane <jamesbane0421@gmail.com>
2020-11-11 10:54:39 -0500

    add get basic token api

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,16 @@
+from sys import path
+
 from django.conf.urls import url, include
 
-from api import views
 import routing.api.urls
 import tools.api.urls
+from rest_framework import views
+
+from api.views import RootAPIView, CustomAuthToken
 
 urlpatterns = [
-    url(r'^$', views.RootAPIView.as_view(), name='index'),
+    url(r'^$', RootAPIView.as_view(), name='index'),
     url(r'^routing/', include(routing.api.urls)),
     url(r'^tools/', include(tools.api.urls)),
+    url(r'^api-token-auth/', CustomAuthToken.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
+from base64 import b64encode
+
+from django.middleware.csrf import get_token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework.views import APIView
@@ -11,3 +16,17 @@ class RootAPIView(APIView):
             'tools': reverse('api:tools-root', request=request, format=format),
         }
         return Response(context)
+
+
+class CustomAuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        credentials = f"{request.data['username']}:{request.data['password']}"
+        encodedCredentials = str(b64encode(credentials.encode("utf-8")), "utf-8")
+        return Response({
+            'token': encodedCredentials,
+        })
+
